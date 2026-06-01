9443f0ae8ec738775caa977c3e0a827e09fb01ad
Orinakter <orin.akter9013@gmail.com>
2026-06-01 18:27:48 +0600

    root route created successfully

---
+++ src/express_server_ts.ts
@@ -0,0 +1,19 @@
+import express, { type Application, type Request, type Response } from "express";
+
+const app:Application = express();
+const port = 5000;
+
+// root route
+
+app.get ("/",(req:Request,res:Response)=>{
+    res.status(200).json({
+    success: true,
+    message: "This is root route"
+});
+
+})
+
+
+app.listen(port,()=>{
+    console.log(`This server is running in port:${port}`);
+})
