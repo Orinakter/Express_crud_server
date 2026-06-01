3ac33278370cb43ba853878e27886d5ad542c03c
Orinakter <orin.akter9013@gmail.com>
2026-06-02 00:49:54 +0600

    -user update done

--- src/express_server_ts.ts
+++ src/express_server_ts.ts
@@ -72,6 +72,48 @@ app.get("/api/users/:id",async(req: Request, res: Response)=>{
 })
 
 
+// update users
+
+app.patch("/api/updateUsers/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { name, email, password, role, age } = req.body;
+     const result = await pool.query(
+      `
+      UPDATE users
+      SET
+        name = COALESCE($1, name),
+        email = COALESCE($2, email),
+        password = COALESCE($3, password),
+        role = COALESCE($4, role),
+        age = COALESCE($5, age),
+        updated_at = CURRENT_TIMESTAMP
+        WHERE id = $6
+        RETURNING *;
+      `,
+      [name, email, password, role, age, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+
+  res.status(200).json({
+    success: true,
+    message: "user Update successfully!",
+    data: result.rows[0],
+  });
+});
+
+
+
+
+
+
+
 app.listen(port, () => {
     initDb()
   console.log(`This server is running in port:${port}`);
